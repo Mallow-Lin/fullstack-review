b7b80dbd9e07a8667c4c242edb5167cc71abac87
Mallow <linlam0923@gmail.com>
2023-02-01 10:57:24 -0500

    Complete save function for database

--- database/index.js
+++ database/index.js
@@ -7,10 +7,27 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repos) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  repos.forEach((repo) => {
+    var newRepo = new Repo ({
+      repo_id: repo.id,
+      repo_name: repo.name,
+      owner: repo.owner.login,
+      description: repo.description,
+      url: repo.url,
+      forks: repo.forks
+    })
+    newRepo.save((err)=> {
+      if (err) {
+        console.log('error in savinv', err)
+      } else {
+        console.log('save successfully');
+      }
+    })
+  })
 }
 
 module.exports.save = save;
